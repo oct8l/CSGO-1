58d7efa1a5fa2f1d74bdadb79a7dfc39bb0a2cf7
oct8l <gitmail@oct8l.email>
2020-02-15 16:38:44 -0600

    Add +ip 0.0.0.0 to allow rcon access

--- etc/entry.sh
+++ etc/entry.sh
@@ -6,6 +6,6 @@ rm -rf /home/steam/csgo
 
 ${STEAMCMDDIR}/steamcmd.sh +login anonymous +force_install_dir ${CSINSTDIR} +app_update ${STEAMAPPID} +quit
 
-${CSINSTDIR}/srcds_run -game csgo -console -usercon -autoupdate -steam_dir ${STEAMCMDDIR} -steamcmd_script ${STEAMAPPDIR}/csgo_update.txt -usercon +fps_max $SRCDS_FPSMAX \
+${CSINSTDIR}/srcds_run -game csgo -console -usercon +ip 0.0.0.0 -autoupdate -steam_dir ${STEAMCMDDIR} -steamcmd_script ${STEAMAPPDIR}/csgo_update.txt -usercon +fps_max $SRCDS_FPSMAX \
 			-tickrate $SRCDS_TICKRATE -port $SRCDS_PORT +tv_port $SRCDS_TV_PORT +clientport $SRCDS_CLIENT_PORT -maxplayers_override $SRCDS_MAXPLAYERS +game_type $SRCDS_GAMETYPE +game_mode $SRCDS_GAMEMODE \
 			+mapgroup $SRCDS_MAPGROUP +map $SRCDS_STARTMAP +sv_setsteamaccount $SRCDS_TOKEN +rcon_password $SRCDS_RCONPW +sv_password $SRCDS_PW +sv_region $SRCDS_REGION
